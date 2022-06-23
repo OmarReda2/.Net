215eaa387326559792526f350f161f2f4005d76b
omar <omarredaelsayied@gmail.com>
2022-06-23 17:52:55 +0200

    P01 Operator OverLoading

---
+++ sessions/C#/week 08 - day 01/P01_OP_OverLoading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace P01_OP_OverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Complex c1 = new Complex() { Real = 1, Imaginary = 2 };
+            //Complex c2 = new Complex() { Real = 3, Imaginary = 4 };
+            //Complex c3 = c1 + c2;
+            //Console.WriteLine(c3);
+
+
+            //Complex c1 = new Complex() ;
+            //Complex c2 = new Complex() ;
+            //Complex c3 = c1 + c2;
+            //Console.WriteLine(c3);
+
+
+            //Complex c1 = default;
+            //Complex c2 = new Complex() ;
+            //Complex c3 = c1 + c2;
+            //Console.WriteLine(c3);
+
+
+            //Complex c1 = new Complex() { Imaginary = 2, Real = 1};
+            //Complex c2 = new Complex();
+            //c1 = c1 + c1;
+            //Console.WriteLine(c1);
+
+
+            //Complex c1 = new Complex() { Imaginary = 2, Real = 1 };
+            //Complex c2 = new Complex();
+            //c1++;
+            //Console.WriteLine(c1);
+
+
+            //Complex c1 = new Complex() { Imaginary = 2, Real = 1 };
+            //Complex c2 = new Complex() { Imaginary = 3, Real = 4 };
+
+            //if (c1 > c2)
+            //    Console.WriteLine("C1 is greater than C2");
+            //else
+            //    Console.WriteLine("C2 is greater than C1");
+
+
+            Complex c1 = new Complex() { Imaginary = 2, Real = 1 };
+            Complex c2 = new Complex() { Imaginary = 3, Real = 4 };
+            int number = c1;
+            string str = (string)c2;
+            Console.WriteLine(number);
+            Console.WriteLine(str);
+
+
+
+        }
+    }
+}
