6830468fdcdeb5c4b2e2851e48f21db8fe8eaa60
omar <omarredaelsayied@gmail.com>
2022-06-23 19:08:58 +0200

    P02 Static & Constant

---
+++ sessions/C#/week 08 - day 01/P02_Static_Constant/Utility.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_Static_Constant
+{
+    public class Utility
+    {
+        public int X{ get; set; }
+        public int Y{ get; set; }
+
+        public Utility(int _X, int _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+
+
+
+
+
+
+        //// Static constructor
+        //static Utility()
+        //{
+        //    Pi = 3.14;
+        //}
+
+        //// Static Attribute
+        //static double Pi;
+
+
+
+
+
+
+
+
+        // Constant
+        const double Pi = 3.14;
+
+
+
+        // Static method (Class method)
+        public static double CM2Inch(double cm)
+        {
+            return cm / 2.54;
+        }
+
+        public static double CalcCircleAria(double radius)
+        {
+            return Pi * radius * radius;
+        }
+
+    }
+}
