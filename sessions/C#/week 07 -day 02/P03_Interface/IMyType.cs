01f87beafb639d4ec4d4139d0ad3678b0eac7ccd
omar <omarredaelsayied@gmail.com>
2022-06-23 09:57:54 +0200

    Part 03 Interface

---
+++ sessions/C#/week 07 -day 02/P03_Interface/IMyType.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_Interface
+{
+    internal interface IMyType
+    {
+        public int salary { get; set; }
+
+        void fucn01();
+    }
+
+
+
+
+
+
+
+
+    class MyType : IMyType
+    {
+
+
+
+
+        public int salary
+        {
+            set
+            {
+                this.salary = value;
+            }
+            get
+            {
+                return this.salary;
+            }
+        }
+
+        public void fucn01()
+        {
+            Console.WriteLine("func01");
+        }
+
+
+
+
+
+
+    }
+}
