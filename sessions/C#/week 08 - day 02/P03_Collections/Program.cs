068a59e97e554a95a2b515da93a0a4675eb06e30
omar <omarredaelsayied@gmail.com>
2022-06-24 17:46:18 +0200

    Week 08 - Day 02 - P03 Collections ArrayList and Lists

---
+++ sessions/C#/week 08 - day 02/P03_Collections/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+
+namespace P03_Collections
+{
+    class Program
+    {
+        public static int SumArrayList(ArrayList list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += (int)list[i];
+            }
+            return sum;
+        }
+
+
+
+
+
+
+        public static int SumList(List<int> list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+
+            #region Non-Generic Collections
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add(1);
+            //arrayList.Add(2);
+            //arrayList.Add("Ahmed");
+            //arrayList.AddRange(new int[] { 1, 2, 3, 4 });
+
+            //int result = SumArrayList(arrayList);
+            //Console.WriteLine(result);
+            #endregion
+
+
+
+
+
+
+
+
+            #region Generic Collectons [Lists] - List
+            List<int> list = new List<int>();
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+
+            list.Add(1);
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+
+            list.Add(5);
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+
+            //list.Add("Ahmed");
+            list.AddRange(new int[] { 4, 5, 6, 7 });
+
+            int result = SumList(list);
+            Console.WriteLine(result);
+
+            list[2] = 10;
+            Console.WriteLine(list[2]);
+
+            //list[100] = 100;
+
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+            list.TrimExcess();
+            Console.WriteLine($"Size = {list.Count} , Capacity = {list.Capacity}");
+
+
+
+
+            #endregion
+
+        }
+    }
+}
