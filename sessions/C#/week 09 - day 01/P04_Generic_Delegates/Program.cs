d8d3917822e84b6510d7c4ee302ad34321ea19d9
omar <omarredaelsayied@gmail.com>
2022-06-28 17:09:50 +0200

    week 09 - day02 - P04_Generic_Delegates

---
+++ sessions/C#/week 09 - day 01/P04_Generic_Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P04_Generic_Delegates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //List<int> list = Enumerable.Range(0, 100).ToList();
+            //GenericCompareFunDelegate<int, int, bool> compare = CompareFunctions.CompareLessThan;
+            //GenericSortingAlgorithm<int>.BubbleSort(list.ToArray(), compare);
+            //foreach (var item in list)
+            //    Console.WriteLine(item);
+
+
+
+            List<string> names = new List<string>() { "Om", "Omar", "Oma", "Omar reda" };
+            GenericCompareFunDelegate<string, string, bool> compare = CompareFunctions.CompareLength;
+            GenericSortingAlgorithm<string>.BubbleSort(names.ToArray(), compare);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
