51ea059dfcd0dbd86a2e178694431a22eba831cc
omar <omarredaelsayied@gmail.com>
2022-06-28 13:37:21 +0200

    week 09 - day 01 - P01_Delegate_Ex01

---
+++ sessions/C#/week 09 - day 01/P02_Delegate_Ex02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P02_Delegate_Ex02
+{
+
+
+
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] Numbers = { 2, 7, -4, 6, -3, 0, 10 };
+
+            //SotingAlgritm.BubbleSort(Numbers);
+            //foreach (var item in Numbers)
+            //    Console.WriteLine(item);
+
+            //CompareFunDelegate compare= CompareFunctions.CompareGreaterThan;
+
+            //SortingAlgritms.BubbleSort(Numbers, CompareFunctions.CompareGreaterThan);
+            SortingAlgritms.BubbleSort(Numbers, CompareFunctions.CompareLessThan);
+            foreach (var item in Numbers)
+                Console.WriteLine(item);
+
+
+
+
+
+        }
+    }
+}
