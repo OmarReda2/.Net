fcb1bd0920748b0804a208572ed5e9045f4de1a9
omar <omarredaelsayied@gmail.com>
2022-07-01 09:44:41 +0200

    week 03 - day 01 - P03_Cast_Functions

--- sessions/Database/week 03 - day 01/SQLQuery1.sql
+++ sessions/Database/week 03 - day 01/SQLQuery1.sql
@@ -11,6 +11,8 @@ Group by dept_ID
 Having Count(ID) >= 1
 
 
+
+
 -------- Null Functions --------
 -- 1. IsNull 
 select LName
@@ -30,6 +32,42 @@ from Student
 select FName, Coalesce(FName, LName, Address,'No Data Found')
 from Student
 
+
+
+
+
+-------- Casting Functions --------
+-- 1. Convert
+select FName + convert(varchar(10), Age)
+from Student
+
+select name + convert(varchar(10), salary, 2)
+from Instructor
+
+select FName + convert(varchar(10), isnull(Age,0))
+from Student
+
+-- 2. Concate
+select CONCAT(FName, ' ', Age)
+from Student
+
+-- 3. Cast
+select CAST(getdate() as varchar(50))
+
+select CONVERT(varchar(50), getdate(), 101)
+select CONVERT(varchar(50), getdate(), 102)
+select CONVERT(varchar(50), getdate(), 103)
+select CONVERT(varchar(50), getdate(), 110)
+
+-- 4. Format
+select format(getdate(), 'dd-MM-yyyy')
+select format(getdate(), 'ddd MMM yyyy')
+select format(getdate(), 'dddd/MMMM/yyyy')
+select format(getdate(), 'dddd')
+select format(getdate(), 'hh:mm:ss')
+select format(getdate(), 'HH:mm:ss tt')
+select format(getdate(), 'dddd/MMMM/yyyy HH:mm:ss tt')
+select format(getdate(), 'dddd', 'ar')
 
 
 
