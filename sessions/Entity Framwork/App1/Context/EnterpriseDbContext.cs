19c882cb28a12e6d3af880b157844d432d6af9b1
omar <omarredaelsayied@gmail.com>
2022-07-04 13:20:16 +0200

    Entity Framework - week 11 - day 01 - Part 2 Mapping with Convension

---
+++ sessions/Entity Framwork/App1/Context/EnterpriseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using App1.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Context
+{
+    internal class EnterpriseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("data source = .; intial catalog =  EnterpriseDb; integrated security = true");
+            optionsBuilder.UseSqlServer("server = .; database =  EnterpriseDb; integrated security = true");
+        }
+
+            public DbSet<Employee> Employees { get; set; }
+    }
+
+}
---
+++ sessions/Entity Framwork/App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
