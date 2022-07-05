987e78329231ca853ef30763579834209e71e6ce
omar <omarredaelsayied@gmail.com>
2022-07-05 22:51:25 +0200

    ASP.Net core MVC - P03 - Controller & View

---
+++ sessions/ASP.NET Core MVC/Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public EmployeeController(ApplicationDBContext context) // Dependecy Injection for context
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Employees.Include(E => E.Departement).ToArrayAsync();
+            return View(result);
+        }
+    }
+}
