56476db6509030f0c58bc88c438a2c8e0c3fb545
omar <omarredaelsayied@gmail.com>
2022-07-12 23:19:58 +0200

    ASP.NET Core MVC - day 04 - Auth2

--- sessions/ASP.NET Core MVC/day 03 - 04/Demo.PL/Controllers/AccountController.cs
+++ sessions/ASP.NET Core MVC/day 03 - 04/Demo.PL/Controllers/AccountController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo.PL.Models;
+using Demo.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Demo.PL.Controllers
 {
     public class AccountController : Controller
     {
+        public UserManager<ApplicationUser> UserManager { get; }
+        public SignInManager<ApplicationUser> SignInManager { get; }
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
         public IActionResult Register()
         {
             return View();
@@ -17,6 +30,19 @@ namespace Demo.PL.Controllers
         public IActionResult ForgetPassword()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var token = UserManager.GeneratePasswordResetTokenAsync(user);
+                    var passwordresetLink = U
+                }
+            }
         }
 
         public IActionResult CompleteForgetPassword()
