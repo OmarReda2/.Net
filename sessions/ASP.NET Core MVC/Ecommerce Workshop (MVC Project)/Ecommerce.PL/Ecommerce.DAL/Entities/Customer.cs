f1ab128766d905f1b5c1d3e4a9105a970b8e014f
omarr <omarredaelsayied@gmail.com>
2022-08-05 15:29:26 +0200

    Ecommerce Workshop(MVC Project) - Part 02 BLL

---
+++ sessions/ASP.NET Core MVC/Ecommerce Workshop (MVC Project)/Ecommerce.PL/Ecommerce.DAL/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.DAL.Entities
+{
+    public class Customer: BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Order Order { get; set; }
+    }
+}
