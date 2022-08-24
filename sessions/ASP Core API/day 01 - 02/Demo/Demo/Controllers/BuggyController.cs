a1e650a81a8a90e5fd706b7504e2902df91cd509
omarr <omarredaelsayied@gmail.com>
2022-08-24 11:11:57 +0200

    ASP Core API - day 02 Part 1.3

---
+++ sessions/ASP Core API/day 01 - 02/Demo/Demo/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+﻿using Demo.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFoundRequest()
+        {
+            var anything = _context.Products.Find(100);
+            if( anything == null )
+                return NotFound(new ApiResponse(404));
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public IActionResult GetServerError()
+        {
+            var anything = _context.Products.Find(100);
+            var something = anything.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ sessions/ASP Core API/day 01 - 02/Demo/Demo/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
