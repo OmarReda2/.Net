fd0b33a23bb97ee6a67dda7392e37221a3f7d05f
omarr <omarredaelsayied@gmail.com>
2022-08-29 14:45:49 +0200

    ASP Core API - day 03

---
+++ sessions/ASP Core API/day 03/Demo/Demo/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Demo.Dtos;
+using Demo.Errors;
+using Demo.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepository;
+        private readonly IGenericRepository<ProductType> _productTypeRepository;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> productRepository,
+            IGenericRepository<ProductBrand> productBrandRepository,
+            IGenericRepository<ProductType> productTypeRepository, 
+            IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _productBrandRepository = productBrandRepository;
+            _productTypeRepository = productTypeRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ProductToReturnDto>> GetProducts([FromQuery]ProductSpecParams productParams)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productRepository.CountAsync(countSpec);
+
+            var products = await _productRepository.ListAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
+        }
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productRepository.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
+
+            return _mapper.Map<Product, ProductToReturnDto>(product);
+        }
+        [HttpGet]
+        [Route("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepository.ListAllAsync());
+        }
+        [HttpGet]
+        [Route("types")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductTypes()
+        {
+            return Ok(await _productTypeRepository.ListAllAsync());
+        }
+    }
+}
---
+++ sessions/ASP Core API/day 03/Demo/Demo/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Demo.Errors
+{
+    public class ApiValidationErrorResponse: ApiException
+    {
+        public ApiValidationErrorResponse() : base(400)
+        {
+
+        }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
