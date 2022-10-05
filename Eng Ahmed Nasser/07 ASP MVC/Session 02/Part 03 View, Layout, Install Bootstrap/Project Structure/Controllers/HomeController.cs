8898b36db86b2da9d5344ef1c8713fda518189d3
Omar Reda <omarredaelsayied@gmail.com>
2022-10-05 16:40:41 +0200

    S2 - Part 03 View, Layout, Install Bootstrap

---
+++ Eng Ahmed Nasser/07 ASP MVC/Session 02/Part 03 View, Layout, Install Bootstrap/Project Structure/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Structure.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
