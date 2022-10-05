1ff1c4944aa185d17e9a5e7e9020672b2c253fd7
Omar Reda <omarredaelsayied@gmail.com>
2022-10-05 16:45:58 +0200

    S2 - Part 08 Presentation Layer

---
+++ Eng Ahmed Nasser/07 ASP MVC/Session 02/Part 08 Presentation Layer/Demo/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Demo.BLL.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly DepartmentRepository departmentRepository;
+
+        public DepartmentController(DepartmentRepository departmentRepository) // configure DI in Startup
+        {
+            this.departmentRepository = departmentRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(departmentRepository.GetAll()); 
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
