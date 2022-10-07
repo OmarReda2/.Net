725ae3cb495701b16de5cbc8cf7589076225884c
Omar Reda <omarredaelsayied@gmail.com>
2022-10-07 14:50:49 +0200

    S3 - Part 06 Employee Repository, Generic Repository

---
+++ Eng Ahmed Nasser/07 ASP MVC/Session 03/Part 06 Employee Repository, Generic Repository/Demo.BLL/Repository/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Demo.BLL.Interface;
+using Demo.DAL.Contexts;
+using Demo.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Repository
+{
+    public class EmployeeRepository:GenericRepository<Employee>, IEmployeeRepository
+    {
+        private readonly MVCAppContext _context;
+
+        public EmployeeRepository(MVCAppContext context):base(context)
+        {
+            
+        }
+
+        public IEnumerable<Employee> GetEmployeesByDepartmentName(string departmentName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
