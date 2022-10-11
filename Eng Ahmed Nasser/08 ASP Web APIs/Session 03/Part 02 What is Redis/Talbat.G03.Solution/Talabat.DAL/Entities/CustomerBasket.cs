37316227cfc015d089b98e09c20ef8cad01297f8
Omar Reda <omarredaelsayied@gmail.com>
2022-10-11 13:02:58 +0200

    S3 - Part 02 What is Redis

---
+++ Eng Ahmed Nasser/08 ASP Web APIs/Session 03/Part 02 What is Redis/Talbat.G03.Solution/Talabat.DAL/Entities/CustomerBasket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.DAL.Entities
+{
+    // ... p2.3
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; }
+    }
+    //p2.4 install StackExchange.Redis in DAL
+    //p2.5 go to Startup to allow DI ...
+}
---
+++ Eng Ahmed Nasser/08 ASP Web APIs/Session 03/Part 02 What is Redis/Talbat.G03.Solution/TextFile1.cs
@@ -0,0 +1,8 @@
+#region p1
+// go to productSpecParams
+#endregion
+
+#region p2
+// create Talabat.DAL.Entities.BasketItem
+// go to Talabat.DAL.Entities.BasketItem ...
+#endregion
