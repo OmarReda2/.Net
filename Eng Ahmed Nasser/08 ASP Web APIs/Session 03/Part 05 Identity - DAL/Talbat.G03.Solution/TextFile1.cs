77beca8aaeb97e352e8866e6406d898eeda16a9f
Omar Reda <omarredaelsayied@gmail.com>
2022-10-12 21:58:47 +0200

    S3 - Part 05 Identity - DAL

---
+++ Eng Ahmed Nasser/08 ASP Web APIs/Session 03/Part 05 Identity - DAL/Talbat.G03.Solution/TextFile1.cs
@@ -0,0 +1,24 @@
+#region p1
+// go to productSpecParams
+#endregion
+
+#region p2
+// create Talabat.DAL.Entities.BasketItem
+// go to Talabat.DAL.Entities.BasketItem ...
+#endregion
+
+#region p3
+// p3.1 create Talabat.BLL.Interfaces.IBasketRepository
+// p3.1 go to Talabat.BLL.Interfaces.IBasketRepository...
+#endregion
+
+#region p4
+// create Controllers.BaskerController
+// p4.1 go to Controllers.BasketController ...
+#endregion
+
+#region p5
+// p5.1 install Microsoft.AspNetCore.Identity.EntityFrameworkCore package in DAL
+// p5.1 create DAL.Entities.Identity.AppUser
+// p5.1 go to DAL.Entities.Identity.AppUser
+#endregion
